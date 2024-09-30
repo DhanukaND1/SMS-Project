2ef6ff351fcadc7759bb09b2a8ae5a5f4c51c632
Dhanuka <2022t01555@stu.cmb.ac.lk>
2024-09-30 11:16:26 +0530

    Added icons to input fields

--- Frontend/src/Components/Login/Login.jsx
+++ Frontend/src/Components/Login/Login.jsx
@@ -9,10 +9,19 @@ const Login = () => {
       <form action="">
         <h2>Login</h2>
         <Link className='xbtn' to="/"><i className="fa-solid fa-xmark"></i></Link>
+
+        <div className="input-wrapper">
         <label htmlFor="">Username</label>
+        <i className="fa-solid fa-user"></i>
         <input type="text" required autoFocus/>
+        </div>
+
+        <div className="input-wrapper">
         <label htmlFor="">Password</label>
+        <i className="fa-solid fa-lock"></i>
         <input type="password" required />
+        </div>
+
         <button>Login</button>
         <h4>Don't Have An Account?<Link to="/Role" className='link'>Sign Up</Link></h4>
       </form>
