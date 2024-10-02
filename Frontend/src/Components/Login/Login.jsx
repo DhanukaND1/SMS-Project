72c8768a0e7ada1d6d2a3922349cda3abe371816
Dhanuka <2022t01555@stu.cmb.ac.lk>
2024-10-02 16:10:30 +0530

    Added role selecting and error handling

--- Frontend/src/Components/Login/Login.jsx
+++ Frontend/src/Components/Login/Login.jsx
@@ -1,14 +1,44 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Login.css'
 import { Link } from 'react-router-dom'
 
 const Login = () => {
+
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [role,setRole] = useState('');
+  const [error,setError] = useState('');
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
+
+  const handleCheck = (e) => {
+    setRole(e.target.value);
+    setError('');
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if(!role){
+      setError("Please select a role.")
+      return;
+    }
+  };
+
   return (
     <div className='cont'>
       
-      <form action="">
+      <form action="" onSubmit={handleSubmit}>
         <h2>Login</h2>
         <Link className='xbtn' to="/"><i className="fa-solid fa-xmark"></i></Link>
+        <label htmlFor="">Select your role</label>
+
+        <div className='role-wrapper'>
+          <input type="radio" name='role' value= "Student" onChange={handleCheck} />Student
+          <input type="radio" name='role' value= "Mentor" onChange={handleCheck} />Mentor
+        </div>
+        <span className='error'>{error}</span>
 
         <div className="input-wrapper">
         <label htmlFor="">Username</label>
@@ -19,12 +49,21 @@ const Login = () => {
         <div className="input-wrapper">
         <label htmlFor="">Password</label>
         <i className="fa-solid fa-lock"></i>
-        <input type="password" required />
+        
+        <div className="eye-wrapper">
+        <span className="icon" onClick={togglePasswordVisibility}>
+            <i className={`fa-solid ${passwordVisible ? 'fa-eye-slash' : 'fa-eye'}`}></i>
+          </span>
         </div>
 
+        <input type={passwordVisible ? 'text' : 'password'} required />
+        </div>
+
+        <div className="btn-center">
         <button>Login</button>
         <h4>Don't Have An Account?<Link to="/Role" className='link'>Sign Up</Link></h4>
         <Link to="/ForgotPassword"><h4>Forgot Password?</h4></Link>
+        </div>
       </form>
     </div>
   )
