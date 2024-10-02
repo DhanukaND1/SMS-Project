02da1b85b4d801a49e34b10365490fd3080d5723
Dhanuka <2022t01555@stu.cmb.ac.lk>
2024-10-02 18:47:25 +0530

    Added to send login info to backend and created error showing of login

--- Frontend/src/Components/Login/Login.jsx
+++ Frontend/src/Components/Login/Login.jsx
@@ -1,30 +1,80 @@
 import React, {useState} from 'react'
 import './Login.css'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Login = () => {
 
   const [passwordVisible, setPasswordVisible] = useState(false);
-  const [role,setRole] = useState('');
-  const [error,setError] = useState('');
+  const [error,setError] = useState({role:'',user:''});
+  const [formData,setFormData] = useState({
+    role:'',
+    user:'',
+    pass:''
+  });
+
+  const navigate = useNavigate();
 
   const togglePasswordVisibility = () => {
     setPasswordVisible(!passwordVisible);
   };
 
   const handleCheck = (e) => {
-    setRole(e.target.value);
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+
+    if (e.target.name === 'role') {
+      setError(prevState => ({ ...prevState, role: '' }));
+    } else if (e.target.name === 'user') {
+      setError(prevState => ({ ...prevState, user: '' }));
+    }
+
     setError('');
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if(!role){
-      setError("Please select a role.")
+    if(!formData.role){
+      setError(prevState => ({ ...prevState, role: 'Please select a role.' }));
       return;
     }
+
+    try {
+      const response = await fetch("http://localhost:5001/api/login",{
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await response.json();
+      if(data.success){
+        alert("Login successful!");
+        setFormData({
+          role:'',
+          user:'',
+          pass:''
+        });
+
+        setError({ role: '', user: '' });
+         
+        if(formData.role==="Student"){
+          navigate('/student-dashboard');
+        }
+        else if(formData.role === "Mentor"){
+          navigate('/mentor-dashboard');
+        }
+      }else{
+        setError(prevState => ({ ...prevState, user: data.message }));
+      }
+    } catch (error) {
+      alert('Error occurred while loging in. Please try again later.');
+      console.error('Error:', error);
+    }
   };
+
+
 
   return (
     <div className='cont'>
@@ -38,12 +88,12 @@ const Login = () => {
           <input type="radio" name='role' value= "Student" onChange={handleCheck} />Student
           <input type="radio" name='role' value= "Mentor" onChange={handleCheck} />Mentor
         </div>
-        <span className='error'>{error}</span>
+        <span className='error'>{error.role}</span>
 
         <div className="input-wrapper">
         <label htmlFor="">Username</label>
         <i className="fa-solid fa-user"></i>
-        <input type="text" required autoFocus/>
+        <input type="text" name='user' value={formData.user} onChange={handleCheck} required autoFocus/>
         </div>
 
         <div className="input-wrapper">
@@ -56,13 +106,15 @@ const Login = () => {
           </span>
         </div>
 
-        <input type={passwordVisible ? 'text' : 'password'} required />
+        <input type={passwordVisible ? 'text' : 'password'} name='pass' value={formData.pass} onChange={handleCheck} required />
         </div>
+
+        <span className="error">{error.user}</span>
 
         <div className="btn-center">
         <button>Login</button>
-        <h4>Don't Have An Account?<Link to="/Role" className='link'>Sign Up</Link></h4>
-        <Link to="/ForgotPassword"><h4>Forgot Password?</h4></Link>
+        <h4>Don't Have An Account?<Link to="/role" className='link'>Sign Up</Link></h4>
+        <Link to="/forgot-password"><h4>Forgot Password?</h4></Link>
         </div>
       </form>
     </div>
