707b2cf194f21a6e0cb9cbed6449f5b328b40b06
sehanmindula <2022t01533@stu.cmb.ac.lk>
2024-10-06 17:10:25 +0530

    Add files via upload

---
+++ Frontend/src/Components/StudentDashboard/Studentdash.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import StudentNav from './StudentNav'
+import Studenthero from './Studenthero'
+
+function Studentdash() {
+  return (
+    <div>
+      <StudentNav />
+      <Studenthero />
+    </div>
+  )
+}
+
+export default Studentdash
