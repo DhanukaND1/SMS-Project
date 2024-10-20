e023b298f474d8680a2700d5f8d2f4955a326d94
Dhanuka <2022t01555@stu.cmb.ac.lk>
2024-10-20 20:10:10 +0530

    Commented unwanted codes

--- Backend/index.js
+++ Backend/index.js
@@ -63,88 +63,88 @@ app.listen(process.env.PORT, () => {
 });
 
 // Mentor Signup
-app.post('/Mentorsignup', async (req, res) => {
-    try {
-        const { id, name, department, email, contactnum, username, password } = req.body;
-        const existingUser = await UserModel.findOne({ email });
-        if (existingUser) {
-            return res.status(400).json({ error: 'Email already exists' });
-        }
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const newUser = new UserModel({ id, name, department, email, contactnum, username, password: hashedPassword });
-        const savedUser = await newUser.save();
-        res.status(201).json(savedUser);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
+// app.post('/Mentorsignup', async (req, res) => {
+//     try {
+//         const { id, name, department, email, contactnum, username, password } = req.body;
+//         const existingUser = await UserModel.findOne({ email });
+//         if (existingUser) {
+//             return res.status(400).json({ error: 'Email already exists' });
+//         }
+//         const hashedPassword = await bcrypt.hash(password, 10);
+//         const newUser = new UserModel({ id, name, department, email, contactnum, username, password: hashedPassword });
+//         const savedUser = await newUser.save();
+//         res.status(201).json(savedUser);
+//     } catch (error) {
+//         res.status(500).json({ error: error.message });
+//     }
+// });
 
 // Get Mentors
-app.get('/mentors', async (req, res) => {
-    const department = req.query.department;
-    try {
-        const mentors = department
-            ? await UserModel.find({ department }, 'name') // Filter by department if provided
-            : await UserModel.find({}, 'name'); // Otherwise, return all mentors
-        res.status(200).json(mentors);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
+// app.get('/mentors', async (req, res) => {
+//     const department = req.query.department;
+//     try {
+//         const mentors = department
+//             ? await UserModel.find({ department }, 'name') // Filter by department if provided
+//             : await UserModel.find({}, 'name'); // Otherwise, return all mentors
+//         res.status(200).json(mentors);
+//     } catch (error) {
+//         res.status(500).json({ error: error.message });
+//     }
+// });
 
 // Student Signup
-app.post('/Studentsignup', async (req, res) => {
-    try {
-        const { id, name, email, batchyear, department, mentor, username, password } = req.body;
-        const existingStudent = await StudentModel.findOne({ email });
-        if (existingStudent) {
-            return res.status(400).json({ error: 'Email already exists' });
-        }
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const newStudent = new StudentModel({ id, name, email, batchyear, department, mentor, username, password: hashedPassword });
-        const savedStudent = await newStudent.save();
-        res.status(201).json(savedStudent);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
+// app.post('/Studentsignup', async (req, res) => {
+//     try {
+//         const { id, name, email, batchyear, department, mentor, username, password } = req.body;
+//         const existingStudent = await StudentModel.findOne({ email });
+//         if (existingStudent) {
+//             return res.status(400).json({ error: 'Email already exists' });
+//         }
+//         const hashedPassword = await bcrypt.hash(password, 10);
+//         const newStudent = new StudentModel({ id, name, email, batchyear, department, mentor, username, password: hashedPassword });
+//         const savedStudent = await newStudent.save();
+//         res.status(201).json(savedStudent);
+//     } catch (error) {
+//         res.status(500).json({ error: error.message });
+//     }
+// });
 
 // Login Route
-app.post('/login', async (req, res) => {
-    try {
-        const { email, password } = req.body;
+// app.post('/login', async (req, res) => {
+//     try {
+//         const { email, password } = req.body;
 
-        //Check if the user is a mentor first
-        const mentor = await UserModel.findOne({ email });
-        if (mentor) {
-            const passwrodMatch = await bcrypt.compare(password, mentor.password);
-            if (passwrodMatch) {
-                req.session.user = { name: mentor.name, email: mentor.email, role: 'mentor' };
-                req.session.save();
-                return res.json({ message: "Success", role: 'mentor' });
-            } else {
-                return res.status(401).json({ error: "Password does not match!" });
-            }
-        }
+//         //Check if the user is a mentor first
+//         const mentor = await UserModel.findOne({ email });
+//         if (mentor) {
+//             const passwrodMatch = await bcrypt.compare(password, mentor.password);
+//             if (passwrodMatch) {
+//                 req.session.user = { name: mentor.name, email: mentor.email, role: 'mentor' };
+//                 req.session.save();
+//                 return res.json({ message: "Success", role: 'mentor' });
+//             } else {
+//                 return res.status(401).json({ error: "Password does not match!" });
+//             }
+//         }
 
-        //If no Mentor, check if the user is a student
-        const student = await StudentModel.findOne({ email });
-        if (student) {
-            const passwrodMatch = await bcrypt.compare(password, student.password);
-            if (passwrodMatch) {
-                req.session.user = { name: student.name, email: student.email, role: 'student' };
-                req.session.save();
-                return res.json({ message: "Success", role: "student" });
-            } else {
-                return res.status(401).json({ error: "Password does not match!" });
-            }
-        }
-        return res.status(401).json({ error: "No user found" });
-    } catch (error) {
-        console.error('Error in /login:', error);  // This will log the error to the console
-        res.status(500).json({ error: 'Internal server error' });
-    }
-});
+//         //If no Mentor, check if the user is a student
+//         const student = await StudentModel.findOne({ email });
+//         if (student) {
+//             const passwrodMatch = await bcrypt.compare(password, student.password);
+//             if (passwrodMatch) {
+//                 req.session.user = { name: student.name, email: student.email, role: 'student' };
+//                 req.session.save();
+//                 return res.json({ message: "Success", role: "student" });
+//             } else {
+//                 return res.status(401).json({ error: "Password does not match!" });
+//             }
+//         }
+//         return res.status(401).json({ error: "No user found" });
+//     } catch (error) {
+//         console.error('Error in /login:', error);  // This will log the error to the console
+//         res.status(500).json({ error: 'Internal server error' });
+//     }
+// });
 
 // Get current logged-in user (either mentor or student)
 app.get('/dashboard', async (req, res) => {
@@ -181,21 +181,21 @@ app.get('/dashboard', async (req, res) => {
 });
 
 //Get Student by Mentor and Batch Year
-app.get('/students', async (req, res) => {
-    const { mentorName, batchyear } = req.query; //Expect mentorName and batchyear a query parameters
+// app.get('/students', async (req, res) => {
+//     const { mentorName, batchyear } = req.query; //Expect mentorName and batchyear a query parameters
 
-    try {
-        //Fetch students assigned to the specific mentor and batch year
-        const students = await StudentModel.find({ mentor: mentorName, batchyear });
+//     try {
+//         //Fetch students assigned to the specific mentor and batch year
+//         const students = await StudentModel.find({ mentor: mentorName, batchyear });
 
-        if (students.length === 0) {
-            return res.status(404).json({ message: 'No student found for this mentor and batch year.' });
-        }
-        res.status(200).json(students);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-})
+//         if (students.length === 0) {
+//             return res.status(404).json({ message: 'No student found for this mentor and batch year.' });
+//         }
+//         res.status(200).json(students);
+//     } catch (error) {
+//         res.status(500).json({ error: error.message });
+//     }
+// })
 
 // Route to serve static files from the 'uploads' folder
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
