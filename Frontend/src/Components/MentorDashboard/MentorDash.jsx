958183c37b7d2eac18c13d7bd126a2c240a7218e
Dhanuka <2022t01555@stu.cmb.ac.lk>
2024-10-20 20:13:08 +0530

    Added sidebar component

--- Frontend/src/Components/MentorDashboard/MentorDash.jsx
+++ Frontend/src/Components/MentorDashboard/MentorDash.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import MentorNav from './MentorNav'
 import MentorHero from './MentorHero'
 import Footer from '../Footer/Footer'
+import Sidebar from '../Sidebar/Sidebar.jsx'
 import './Mentor.css'
 
 function MentorDash() {
     return (
         <div>
-            <MentorNav />
+            <Sidebar />
+            {/* <MentorNav /> */}
             <MentorHero />
-            {/* <Footer /> */}
+            <Footer />
         </div>
     )
 }
