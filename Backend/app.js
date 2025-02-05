16c74c78b519f4238078d7ec76eebef8250aab80
Dhanuka <2022t01555@stu.cmb.ac.lk>
2025-02-05 14:37:53 +0530

    Created get mentor data route

--- Backend/app.js
+++ Backend/app.js
@@ -224,7 +224,7 @@ app.post('/api/login', async (req, res) => {
 app.get('/api/dashboard', async (req, res) => {
 
   if (req.session.user) {
-      const { name, email, role } = req.session.user;
+      const { email, role } = req.session.user;
       if (role === 'Mentor') {
 
           // Return mentor-specific response
@@ -778,6 +778,27 @@ app.post('/api/notifications/read', async (req, res) => {
   }
 });
 
+// route for getting mentor data
+app.get('/api/mentor-data', async (req,res) => {
+
+  const {mentor} = req.query;
+  try{
+    const user = await Mentor.findOne({name: mentor});
+    console.log(user);
+    if(user){
+      res.status(200).json({
+        name: user.name,
+        email: user.mail,
+        dept: user.dept,
+        phone: user.phone,
+        image: user.image 
+      })
+    } 
+  }catch(err){
+    console.error('Error fetching mentor data', err.message );
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
 // Start the server
 const port = process.env.PORT1 || 5001;
