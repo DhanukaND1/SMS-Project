4166007a2111b9c0fc3e3ff26069f8afd17313ca
Dhanuka <2022t01555@stu.cmb.ac.lk>
2025-02-08 18:51:45 +0530

    Added new route for storing feedbacks

--- Backend/app.js
+++ Backend/app.js
@@ -11,6 +11,7 @@ const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const { start } = require('repl');
 const { type } = require('os');
+// const { default: Feedback } = require('../Frontend/src/Components/Feedback/Feedback');
 require('dotenv').config();
 // const Notification = require('./model/Notification');
 
@@ -116,6 +117,14 @@ const SessionInfo = mongoose.model('SessionInfo', new mongoose.Schema({
     Date: {type:Date, required:true},
     SessionMode: {type:String, required:true},
     AdditionalNote: {type:String, required:false}
+}));
+
+//collection for Feedbacks
+
+const Feedback = mongoose.model('Feedback', new mongoose.Schema({
+  Mentor: {type:String, required:true},
+  Description: {type:String, required:true},
+  Date: {type:Date, required:true}
 }));
 
 //endpoint for signup student
@@ -962,6 +971,19 @@ app.get('/api/ongoing-sessions', async (req, res) => {
   }catch (error) {
     console.error("Error fetching ongoin sessions:", error);
     res.status(500).json({ error: 'Internal server error'});
+  }
+});
+
+app.post('/api/feedback', async (req, res) =>{
+  try{
+    const {mentorName, description, date} = req.body;
+
+    const newFeedback = new Feedback({Mentor: mentorName, Description: description, Date: date});
+    await newFeedback.save();
+    res.status(200).json({success: true, message: 'Feedback submitted successfully!'});
+  }catch(error){
+    console.log('Error during feedback submitting:', error);
+    res.status(500).json({ success: false, message: 'Error submitting feedback.', error: error.message });
   }
 });
 
